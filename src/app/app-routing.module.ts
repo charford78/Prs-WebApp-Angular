7bd7a63267f944b7c412a23b87d173adfebd6185
Charlie Ford <charles.ford1978@gmail.com>
2021-11-24 15:00:57 -0500

    Added more paths to the routing modules for the new components.  Edited the app.module to account for renaming a couple of components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './misc/about/about.component';
 import { HomeComponent } from './misc/home/home.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { RequestlinesCreateComponent } from './requestlines/requestlines-create/requestlines-create.component';
+import { RequestCreateComponent } from './requests/request-create/request-create.component';
+import { RequestDetailComponent } from './requests/request-detail/request-detail.component';
+import { RequestEditComponent } from './requests/request-edit/request-edit.component';
+import { RequestListComponent } from './requests/request-list/request-list.component';
+import { RequestReviewComponent } from './requests/request-review/request-review.component';
+import { RequestReviewslistComponent } from './requests/request-reviewslist/request-reviewslist.component';
 import { UserCreateComponent } from './users/user-create/user-create.component';
 import { UserDetailComponent } from './users/user-detail/user-detail.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { UserLoginComponent } from './users/user-login/user-login.component';
+import { VendorCreateComponent } from './vendors/vendor-create/vendor-create.component';
+import { VendorDetailComponent } from './vendors/vendor-detail/vendor-detail.component';
+import { VendorEditComponent } from './vendors/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './vendors/vendor-list/vendor-list.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "", pathMatch: "full" },
@@ -17,6 +32,22 @@ const routes: Routes = [
   { path: "users/edit/:id", component: UserEditComponent},
   { path: "users/create", component: UserCreateComponent},
   { path: "users/login", component: UserLoginComponent},
+  { path: "vendors/list", component: VendorListComponent},
+  { path: "vendors/details/:id", component: VendorDetailComponent},
+  { path: "vendors/create", component: VendorCreateComponent},
+  { path: "vendors/edit/:id", component: VendorEditComponent},
+  { path: "products/list", component: ProductListComponent},
+  { path: "products/details/:id", component: ProductDetailComponent},
+  { path: "products/create", component: ProductCreateComponent},
+  { path: "products/edit/:id", component: ProductEditComponent},
+  { path: "requests/list", component: RequestListComponent},
+  { path: "requests/details/:id", component: RequestDetailComponent},
+  { path: "requests/create", component: RequestCreateComponent},
+  { path: "requests/lines/:id", component: RequestEditComponent},
+  { path: "requests/reviews", component: RequestReviewslistComponent},
+  { path: "requests/review/:id", component: RequestReviewComponent},
+  { path: "requestlines/create/:id", component: RequestlinesCreateComponent},
+  { path: "requestlines/edit/:id", component: RequestReviewslistComponent},
   
 
   {path: "**", component: HomeComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { VendorListComponent } from './vendors/vendor-list/vendor-list.component
 import { VendorDetailComponent } from './vendors/vendor-detail/vendor-detail.component';
 import { VendorCreateComponent } from './vendors/vendor-create/vendor-create.component';
 import { VendorEditComponent } from './vendors/vendor-edit/vendor-edit.component';
-import { VendorDeleteComponent } from './vendors/vendor-delete/vendor-delete.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { ProductCreateComponent } from './products/product-create/product-create.component';
@@ -26,7 +25,6 @@ import { RequestListComponent } from './requests/request-list/request-list.compo
 import { RequestDetailComponent } from './requests/request-detail/request-detail.component';
 import { RequestCreateComponent } from './requests/request-create/request-create.component';
 import { RequestEditComponent } from './requests/request-edit/request-edit.component';
-import { RequestReviewsComponent } from './requests/request-reviews/request-reviews.component';
 import { RequestReviewComponent } from './requests/request-review/request-review.component';
 import { RequestReviewslistComponent } from './requests/request-reviewslist/request-reviewslist.component';
 import { RequestlinesCreateComponent } from './requestlines/requestlines-create/requestlines-create.component';
@@ -47,7 +45,6 @@ import { RequestlinesEditComponent } from './requestlines/requestlines-edit/requ
     VendorDetailComponent,
     VendorCreateComponent,
     VendorEditComponent,
-    VendorDeleteComponent,
     ProductListComponent,
     ProductDetailComponent,
     ProductCreateComponent,
@@ -56,7 +53,6 @@ import { RequestlinesEditComponent } from './requestlines/requestlines-edit/requ
     RequestDetailComponent,
     RequestCreateComponent,
     RequestEditComponent,
-    RequestReviewsComponent,
     RequestReviewComponent,
     RequestReviewslistComponent,
     RequestlinesCreateComponent,
