d51e70a5ef4b5a33f561ae4661714e5646922a8d
Charlie Ford <charles.ford1978@gmail.com>
2021-11-30 10:24:53 -0500

    Added Router service to constructor.  Added checkLogin function.

--- src/app/misc/system.service.ts
+++ src/app/misc/system.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,14 @@ export class SystemService {
 
   loggedInUser: any = null;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  checkLogin(): void {
+    if(this.loggedInUser === null) {
+      this.router.navigateByUrl("/users/login")
+    }
+  }
 
 }
