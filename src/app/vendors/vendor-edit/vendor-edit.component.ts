fc3741a0d52ea61200c31993badf9b1a40cea809
Charlie Ford <charles.ford1978@gmail.com>
2021-11-30 14:31:03 -0500

    Added variables, services and functions.

--- src/app/vendors/vendor-edit/vendor-edit.component.ts
+++ src/app/vendors/vendor-edit/vendor-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Vendor } from '../vendor.class';
+import { VendorService } from '../vendor.service';
 
 @Component({
   selector: 'app-vendor-edit',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorEditComponent implements OnInit {
 
-  constructor() { }
+  vendor!: Vendor;
+
+  vendorId: number = 0;
+
+  constructor(
+    private vensvc: VendorService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private syssvc: SystemService
+  ) { }
+
+  save(): Vendor {
+      this.vensvc.change(this.vendor, this.vendorId).subscribe({
+        next: res => {
+          console.debug("Vendor edited:", res);
+          this.router.navigateByUrl("/vendors/list");
+        },
+        error: err => {
+          console.error(err);
+        }
+      });
+    return this.vendor;
+  }
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.vendorId = this.route.snapshot.params["id"];
+
+    this.vensvc.getById(this.vendorId).subscribe({
+      next: res => {
+        console.debug("Vendor:", res);
+        this.vendor = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
