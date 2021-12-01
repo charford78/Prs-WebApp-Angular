cbabc1352c5efba415e87b7f9ebe5fc63a35995c
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 08:55:01 -0500

    Added properties, services and functions.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { VendorListComponent } from 'src/app/vendors/vendor-list/vendor-list.component';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  productId: number = 0;
+
+  constructor(
+    private prdsvc: ProductService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private syssvc: SystemService
+  ) { }
+
+  delete(pid: number): Product {
+    this.productId = pid;
+
+    this.prdsvc.remove(this.productId).subscribe({
+      next: res => {
+        console.debug("Product removed:", res);
+        this.product = res;
+        this.router.navigateByUrl("/products/list");
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+    return this.product;
+  }
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.productId = this.route.snapshot.params["id"];
+
+    this.prdsvc.getById(this.productId).subscribe({
+      next: res => {
+        console.debug("Product:", res);
+        this.product = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
