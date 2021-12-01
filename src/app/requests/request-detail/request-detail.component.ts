a91aaf1f18570e4366069ffaddffbc0fdece63f7
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 14:44:54 -0500

    Added properties, services and functions.

--- src/app/requests/request-detail/request-detail.component.ts
+++ src/app/requests/request-detail/request-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Request } from '../request.class';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-detail',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestDetailComponent implements OnInit {
 
-  constructor() { }
+  request!: Request;
+
+  requestId: number = 0;
+
+  constructor(
+    private reqsvc: RequestService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private syssvc: SystemService
+  ) { }
+
+  delete(rid: number): Request {
+    this.requestId = rid;
+
+    this.reqsvc.remove(this.requestId).subscribe({
+      next: res => {
+        console.debug("Request removed:", res);
+        this.request = res;
+        this.router.navigateByUrl("/requests/list");
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+    return this.request;
+  }
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.requestId = this.route.snapshot.params["id"];
+
+    this.reqsvc.getById(this.requestId).subscribe({
+      next: res => {
+        console.debug("Request:", res);
+        this.request = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
