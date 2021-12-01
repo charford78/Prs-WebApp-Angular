b32d47733e208906fc4796437cfc3c26177f20f5
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 16:38:11 -0500

    Added properties, functions and services.

--- src/app/requests/request-create/request-create.component.ts
+++ src/app/requests/request-create/request-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { User } from 'src/app/users/user.class';
+import { Request } from '../request.class';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-create',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestCreateComponent implements OnInit {
 
-  constructor() { }
+  request: Request = new Request();
+
+  user: User = new User();
+
+  constructor(
+    private reqsvc: RequestService,
+    private router: Router,
+    private syssvc: SystemService,
+  ) { }
+
+  save(): Request {
+      this.request.userId = this.user.id;
+
+      this.reqsvc.create(this.request).subscribe({
+        next: res => {
+          console.debug("New Request created:", res);
+          this.request = res;
+          this.router.navigateByUrl("/requests/list");
+        },
+        error: err => {
+          console.error(err);
+        }
+      });    
+    return this.request;
+  }
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.user = this.syssvc.loggedInUser;
   }
 
 }
