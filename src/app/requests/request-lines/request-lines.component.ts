8a71adae30eaefb88c486645e9d72af7617b04e4
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 16:39:36 -0500

    Added properties, services and functions.

--- src/app/requests/request-lines/request-lines.component.ts
+++ src/app/requests/request-lines/request-lines.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Request } from '../request.class';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-lines',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestLinesComponent implements OnInit {
 
-  constructor() { }
+  request: Request = new Request();
+  requestId: number = 0;
+
+  constructor(
+    private reqsvc: RequestService,
+    private syssvc: SystemService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.requestId = this.route.snapshot.params["id"];
+
+    this.reqsvc.getById(this.requestId).subscribe({
+      next: res => {
+        console.debug("Request:", res);
+        this.request = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
