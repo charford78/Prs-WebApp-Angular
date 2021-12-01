245661bf71cedb765482fbef09b71bc0465bae61
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 16:39:00 -0500

    Added properties, services and functions.

--- src/app/requests/request-edit/request-edit.component.ts
+++ src/app/requests/request-edit/request-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Request } from '../request.class';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-edit',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestEditComponent implements OnInit {
 
-  constructor() { }
+  request!: Request;
+
+  requestId: number = 0;
+
+  constructor(
+    private reqsvc: RequestService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private syssvc: SystemService
+  ) { }
+
+  save(): void {
+    this.reqsvc.change(this.request, this.requestId).subscribe({
+      next: res => {
+        console.debug("Request edited:", this.request)
+        this.router.navigateByUrl("/requests/list");
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.requestId = this.route.snapshot.params["id"];
+
+    this.reqsvc.getById(this.requestId).subscribe({
+      next: res => {
+        console.debug("Request:", res);
+        this.request = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
