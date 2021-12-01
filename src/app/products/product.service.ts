c08430f96b0a25667ba7e298dedbadc3845954ab
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 08:53:09 -0500

    Added functions for http methods.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  baseurl: string = "http://localhost:54800/api/products"
+
+  constructor(
+    private httpsvc: HttpClient
+  ) { }
+
+  list(): Observable<Product[]> {
+    return this.httpsvc.get(`${this.baseurl}`) as Observable<Product[]>;
+  }
+
+  getById(id: number): Observable<Product> {
+    return this.httpsvc.get(`${this.baseurl}/${id}`) as Observable<Product>;
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.httpsvc.post(`${this.baseurl}`, product) as Observable<Product>;
+  }
+
+  change(product: Product, id: number): Observable<Product> {
+    return this.httpsvc.put(`${this.baseurl}/${id}`, product) as Observable<Product>;
+  }
+
+  remove(id: number): Observable<Product> {
+    return this.httpsvc.delete(`${this.baseurl}/${id}`) as Observable<Product>;
+  }
+ 
 }
