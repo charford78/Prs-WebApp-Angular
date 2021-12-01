5b8fe53c2fb173cd7a8b57f1f90facbfb2887025
Charlie Ford <charles.ford1978@gmail.com>
2021-12-01 12:33:12 -0500

    Added properties, services and functions.

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Vendor } from 'src/app/vendors/vendor.class';
+import { VendorService } from 'src/app/vendors/vendor.service';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +13,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  productId: number = 0;
+
+  vendors: Vendor[] = [];
+
+  constructor(
+    private prdsvc: ProductService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private syssvc: SystemService,
+    private vensvc: VendorService
+  ) { }
+
+  save(): Product {
+    this.product.vendorId = +this.product.vendorId;
+    this.prdsvc.change(this.product, this.productId).subscribe({
+      next: res => {
+        console.debug("Product edited:", this.product);
+        this.router.navigateByUrl("/products/list");
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  return this.product;
+}
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.productId = this.route.snapshot.params["id"];
+
+    this.prdsvc.getById(this.productId).subscribe({
+      next: res => {
+        console.debug("Product:", res);
+        this.product = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+
+    this.vensvc.list().subscribe({
+      next: res => {
+        this.vendors = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
