d43784ab9667e93aa7c0afa6d672f03bf3d42974
Charlie Ford <charles.ford1978@gmail.com>
2021-12-02 12:02:42 -0500

    Added html, services, functions and properties.

--- src/app/requestlines/requestlines-create/requestlines-create.component.ts
+++ src/app/requestlines/requestlines-create/requestlines-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Product } from 'src/app/products/product.class';
+import { ProductService } from 'src/app/products/product.service';
+import { Requestlines } from '../requestlines.class';
+import { RequestlinesService } from '../requestlines.service';
 
 @Component({
   selector: 'app-requestlines-create',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestlinesCreateComponent implements OnInit {
 
-  constructor() { }
+  reqline: Requestlines = new Requestlines();
+  products: Product[] = []
+
+  constructor(
+    private rqlsvc: RequestlinesService,
+    private router: Router,
+    private syssvc: SystemService,
+    private route: ActivatedRoute,
+    private prdsvc: ProductService
+  ) { }
+
+  save(): void {
+    this.rqlsvc.create(this.reqline).subscribe({
+      next: res => {
+        console.debug("New Requestline created:", res);
+        this.reqline = res;
+        this.router.navigateByUrl(`/requests/lines/${this.reqline.requestId}`);
+      },
+      error: err => {
+        console.error(err);
+      }
+    });    
+}
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.reqline.requestId = this.route.snapshot.params["id"];
+
+    this.prdsvc.list().subscribe({
+      next: res => {
+        this.products = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
