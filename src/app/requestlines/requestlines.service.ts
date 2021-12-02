aaf444c5dc614765f817a14aa77288f154529c48
Charlie Ford <charles.ford1978@gmail.com>
2021-12-02 12:02:10 -0500

    Added functions for http methods.

--- src/app/requestlines/requestlines.service.ts
+++ src/app/requestlines/requestlines.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Requestlines } from './requestlines.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestlinesService {
 
-  constructor() { }
+  baseurl: string = "http://localhost:54800/api/requestlines";
+
+
+  constructor(
+    private httpsvc: HttpClient
+  ) { }
+
+  create(requestline: Requestlines): Observable<Requestlines> {
+    return this.httpsvc.post(`${this.baseurl}`, requestline) as Observable<Requestlines>;
+  }
+
+  change(requestline: Requestlines, id: number): Observable<Requestlines> {
+    return this.httpsvc.put(`${this.baseurl}/${id}`, requestline) as Observable<Requestlines>;
+  }
+
+  remove(id: number): Observable<Requestlines> {
+    return this.httpsvc.delete(`${this.baseurl}/${id}`) as Observable<Requestlines>;
+  }
+
 }
