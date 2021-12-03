9546b33db3b6c6e6bb2e232edebad4e377f1991c
Charlie Ford <charles.ford1978@gmail.com>
2021-12-03 00:09:02 -0500

    Added html, properties, services and functions.

--- src/app/requestlines/requestlines-edit/requestlines-edit.component.ts
+++ src/app/requestlines/requestlines-edit/requestlines-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Product } from 'src/app/products/product.class';
+import { ProductService } from 'src/app/products/product.service';
+import { Requestlines } from '../requestlines.class';
+import { RequestlinesService } from '../requestlines.service';
 
 @Component({
   selector: 'app-requestlines-edit',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestlinesEditComponent implements OnInit {
 
-  constructor() { }
+  reqline!: Requestlines;
+  reqlineId: number = 0;
+  products: Product[] = [];
+
+  constructor(
+    private rqlsvc: RequestlinesService,
+    private prdsvc: ProductService,
+    private syssvc: SystemService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  save(): void {
+    this.rqlsvc.change(this.reqline, this.reqlineId).subscribe({
+      next: res => {
+        console.debug("Requestline edited:", this.reqline);
+        this.router.navigateByUrl(`/requests/lines/${this.reqline.requestId}`);
+      },
+      error: err => {
+        console.error(err);
+      }
+    });    
+}
+
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.reqlineId = this.route.snapshot.params["id"];
+
+    this.prdsvc.list().subscribe({
+      next: res => {
+        this.products = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+
+    this.rqlsvc.getById(this.reqlineId).subscribe({
+      next: res => {
+        console.debug("Requestline:", res);
+        this.reqline = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
