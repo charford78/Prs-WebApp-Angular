f1dc47235aa82162e8085092c8c5c98f6ca05f46
Charlie Ford <charles.ford1978@gmail.com>
2021-12-03 12:07:21 -0500

    Removed unnecessary variables and services.  Added refresh function and moved page initialization actions from ngOnInit to the refresh body.  Added refresh function to ngOnInit and to review and delete functions and removed router actions from them.  Removed unnecessary variable assignments from functions.

--- src/app/requests/request-lines/request-lines.component.ts
+++ src/app/requests/request-lines/request-lines.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { SystemService } from 'src/app/misc/system.service';
 import { Requestlines } from 'src/app/requestlines/requestlines.class';
 import { RequestlinesService } from 'src/app/requestlines/requestlines.service';
@@ -17,24 +17,20 @@ export class RequestLinesComponent implements OnInit {
   requestId: number = 0;
   requestlines: Requestlines[] = [];
   requestlineId: number = 0;
-  requestline!: Requestlines;
 
   constructor(
     private reqsvc: RequestService,
     private syssvc: SystemService,
     private route: ActivatedRoute,
-    private router: Router,
     private rqlsvc: RequestlinesService
   ) { }
 
   review(req: Request): void {
-    this.request = req;
-    
-    this.reqsvc.review(this.request).subscribe({
+    this.reqsvc.review(req).subscribe({
       next: res => {
         console.debug("Request status changed:", res);
         this.request = res;
-        this.router.navigateByUrl(`/requests/lines/${this.requestId}`);
+        this.refresh();
       },
       error: err => {
         console.error(err);
@@ -42,26 +38,21 @@ export class RequestLinesComponent implements OnInit {
     });
   }
 
-  delete(rlid: number): Requestlines {
+  delete(rlid: number): void {
     this.requestlineId = rlid;
 
     this.rqlsvc.remove(this.requestlineId).subscribe({
       next: res => {
         console.debug("Requestline removed:", res);
-        this.requestline = res;
-        this.router.navigateByUrl(`/requests/lines/${this.requestId}`);
+        this.refresh();
       },
       error: err => {
         console.error(err);
       }
     });
-    return this.requestline;
   }
 
-
-  ngOnInit(): void {
-    this.syssvc.checkLogin();
-
+  refresh(): void {
     this.requestId = this.route.snapshot.params["id"];
 
     this.reqsvc.getById(this.requestId).subscribe({
@@ -74,6 +65,14 @@ export class RequestLinesComponent implements OnInit {
         console.error(err);
       }
     });
+
+  }
+
+  ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.refresh();
+
 
   }
 
