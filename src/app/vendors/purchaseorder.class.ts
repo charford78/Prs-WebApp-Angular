94728d23bb712fc489c1aa4fd1b09f77e7e4a063
Charles Ford <charles.ford1978@gmail.com>
2022-01-08 00:09:33 -0500

    Added purchaseOrderlines array property.

--- src/app/vendors/purchaseorder.class.ts
+++ src/app/vendors/purchaseorder.class.ts
@@ -1,9 +1,11 @@
+import { Purchaseorderline } from "./purchaseorderline.class";
 import { Vendor } from "./vendor.class";
 
 export class Purchaseorder{
 
     vendor: Vendor = new Vendor();
     dateTime: Date = new Date();
+    purchaseOrderLines!: Purchaseorderline[];
     orderTotal: number = 0;
 
     constructor() {        
