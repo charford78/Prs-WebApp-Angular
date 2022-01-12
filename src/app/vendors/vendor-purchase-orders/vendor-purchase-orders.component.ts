8fb0ff6ecca491718f0fa55078a37905386b9b60
Charles Ford <charles.ford1978@gmail.com>
2022-01-12 18:07:19 -0500

    Added services, functions and properties.

--- src/app/vendors/vendor-purchase-orders/vendor-purchase-orders.component.ts
+++ src/app/vendors/vendor-purchase-orders/vendor-purchase-orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Purchaseorder } from '../purchaseorder.class';
+import { VendorService } from '../vendor.service';
 
 @Component({
   selector: 'app-vendor-purchase-orders',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorPurchaseOrdersComponent implements OnInit {
 
-  constructor() { }
+  pord!: Purchaseorder;
+
+  vendorId: number = 0;
+
+  constructor(
+    private syssvc: SystemService,
+    private vensvc: VendorService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.syssvc.checkLogin();
+
+    this.vendorId = this.route.snapshot.params["id"];
+
+    this.vensvc.getPurchaseOrder(this.vendorId).subscribe({
+      next: res => {
+        console.debug("PurchaseOrder:", res);
+        this.pord = res;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 
 }
